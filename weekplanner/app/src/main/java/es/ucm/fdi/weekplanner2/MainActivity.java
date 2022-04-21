e91802a0272a4e61dac0ae0ac7ac644278e1e1fc
danicobos01 <dcobos01@ucm.es>
2022-04-21 17:39:46 +0200

    Subida Tienda de recompensas
    Falta únicamente lo de las monedas

--- weekplanner/app/src/main/java/es/ucm/fdi/weekplanner2/MainActivity.java
+++ weekplanner/app/src/main/java/es/ucm/fdi/weekplanner2/MainActivity.java
@@ -1,14 +1,88 @@
 package es.ucm.fdi.weekplanner2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.PopupWindow;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity /* implements View.OnClickListener*/ {
+
+    private RecyclerView rv;
+    private ArrayList<RecompensaInfo> recArr;
+    private RecompensasAdapter adapter;
+    private DatabaseOrg dbAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ThemeUtils.onActivityCreateSetTheme(this);
         setContentView(R.layout.activity_main);
+
+        dbAdapter = new DatabaseOrg(getApplicationContext());
+        recArr = new ArrayList<RecompensaInfo>();
+        recArr = dbAdapter.leerRecompensasFromDB();
+        adapter = new RecompensasAdapter(recArr, this, dbAdapter);
+
+
+        rv = findViewById(R.id.recycler);
+        rv.setAdapter(adapter);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        adapter.notifyDataSetChanged();
     }
+
+    // Funcion que se llama cuando se pulsa el botón de información en la tienda de recompensas
+    public void onButtonShowPopupWindowClick(View view) {
+
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.popup_window, null);
+
+        // crea la popup window
+        int width = 900;
+        int height = 900;
+        boolean focusable = true;
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        // Muestra la popup window
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+        popupWindow.setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+        // Intento de hacer el popup flotable
+
+
+        // Oscurecer fondo mientras la popup window esté activa
+        View container = (View) popupWindow.getContentView().getParent();
+        WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        WindowManager.LayoutParams p = (WindowManager.LayoutParams) container.getLayoutParams();
+        p.flags |= WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+        p.dimAmount = 0.7f;
+        wm.updateViewLayout(container, p);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            popupWindow.setElevation(20);
+        }
+
+        // Elimina la popup window una vez se toca la pantalla
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
+    }
+
 }
---
+++ weekplanner/app/src/main/java/es/ucm/fdi/weekplanner2/ThemeUtils.java
@@ -0,0 +1,48 @@
+package es.ucm.fdi.weekplanner2;
+
+import android.app.Activity;
+import android.content.Intent;
+
+public class ThemeUtils {
+
+    /*
+    Clase de utilidades para llevar a cabo los cambios de tema
+     */
+
+    public final static int AZUL = 0;
+    public final static int AMARILLO = 1;
+    public final static int VERDE = 2;
+    public final static int ROJO = 3;
+    public final static int MORADO = 4;
+
+    private static int cTheme = MORADO;
+
+    // Función para cambiar el tema de la actividad principal
+    public static void changeToTheme(Activity activity, int theme) {
+        cTheme = theme;
+        activity.finish(); // Termina el main activity anterior y abre otro
+        activity.startActivity(new Intent(activity, activity.getClass()));
+    }
+
+    public static void onActivityCreateSetTheme(Activity activity){
+        switch (cTheme)
+        {
+            case AZUL:
+                activity.setTheme(R.style.Theme2_TiendaDeRecompensas);
+                break;
+            case AMARILLO:
+                activity.setTheme(R.style.Theme3_TiendaDeRecompensas);
+                break;
+            case VERDE:
+                activity.setTheme(R.style.Theme4_TiendaDeRecompensas);
+                break;
+            case ROJO:
+                activity.setTheme(R.style.Theme5_TiendaDeRecompensas);
+                break;
+            default:
+                activity.setTheme(R.style.Theme_TiendaDeRecompensas);
+        }
+
+    }
+
+}
