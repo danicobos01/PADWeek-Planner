fdd787538c1abdbef51b49a5161eb8e3dbd26810
pali509 <pablcamp@ucm.es>
2022-04-27 11:38:19 +0200

    push de alvaro

---
+++ weekplanner/app/src/main/java/es/ucm/fdi/MainActivity.java
@@ -0,0 +1,162 @@
+package es.ucm.fdi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import es.ucm.fdi.Calendario.*;
+import es.ucm.fdi.tienda.TiendaMain;
+
+public class MainActivity extends AppCompatActivity {
+    Button dia;
+    Button semana;
+    Button mes;
+    Button tienda;
+    Button temp;
+    Button estad;
+    private TextView monthYearText;
+    private RecyclerView calendarRecyclerView;
+    private ListView eventListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initWidgets();
+        CalendarUtils.selectedDate = LocalDate.now();
+        setWeekView();
+
+        tienda = (Button) findViewById(R.id.Tienda);
+        tienda.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, TiendaMain.class));
+            }
+        });
+        dia = (Button) findViewById(R.id.Dia);
+        dia.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, DailyCalendarActivity.class));
+            }
+        });
+        semana = (Button) findViewById(R.id.Semana);
+        semana.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
+            }
+        });
+        mes = (Button) findViewById(R.id.Mes);
+        mes.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, MonthCalendarActivity.class));
+            }
+        });
+        temp = (Button) findViewById(R.id.Temporizador);
+        temp.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, TiendaMain.class));
+            }
+        });
+        estad = (Button) findViewById(R.id.Estadisticas);
+        estad.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, TiendaMain.class));
+            }
+        });
+    }
+    private void setWeekView() {
+        monthYearText.setText(monthYearFromDate(CalendarUtils.selectedDate));
+        ArrayList<LocalDate> days = daysInWeekArray(CalendarUtils.selectedDate);
+
+        CalendarAdapter calendarAdapter = new CalendarAdapter(days, this);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
+        calendarRecyclerView.setLayoutManager(layoutManager);
+        calendarRecyclerView.setAdapter(calendarAdapter);
+        setEventAdapter();
+
+    }
+
+    private void initWidgets() {
+        calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
+        monthYearText = findViewById(R.id.monthYearTV);
+        eventListView = findViewById(R.id.eventListView);
+    }
+
+    public void previousWeekAction(View view) {
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.minusWeeks(1);
+        setWeekView();
+    }
+
+    public void nextWeekAction(View view) {
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.plusWeeks(1);
+        setWeekView();
+    }
+
+    @Override
+    public void onItemClick(int position, LocalDate date) {
+        CalendarUtils.selectedDate = date;
+        setWeekView();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setEventAdapter();
+    }
+
+    private void setEventAdapter() {
+        ArrayList<Event> dailyEvents = Event.eventsForDate(CalendarUtils.selectedDate);
+        EventAdapter eventAdapter = new EventAdapter(getApplicationContext(), dailyEvents);
+        eventListView.setAdapter(eventAdapter);
+    }
+
+    public void newEventAction(View view) {
+        startActivity(new Intent(this, EventEditActivity.class));
+    }
+
+
+
+    public void BotonDia(View view) {
+
+    }
+
+    public void BotonSemana(View view) {
+    }
+
+    public void BotonMes(View view) {
+    }
+
+    public void BotonTemporizador(View view) {
+    }
+
+    public void BotonEstad(View view) {
+    }
+
+  /*  public void BotonTienda(View view) {
+        try{
+            Intent i = new Intent(this, TiendaMain.class);
+
+            startActivity(i);
+
+        } catch(Exception e1) {
+            Toast.makeText(this,"No ha funcionado xd", Toast.LENGTH_LONG).show();
+        }
+    }
+
+   */
+}
